b7df6aa3e38598c7e2a20b2c063c7ab696d048f4
Asharamart2075@gmail.com <Asharamart2075@gmail.com>
2023-06-25 12:22:43 -0500

    Resume now displays

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import ContactMe from './components/ContactMe';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Menubar from './components/Menubar';
+import Resume from './components/Resume';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <ContactMe />
       <Skills />
       <Projects />
+      <Resume />
     </div>
   );
 }
