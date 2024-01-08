3fdffc798220e84a90ef5c1ef116d5843f55c7d8
Ashara Martinez <Asharamart2075@gmail.com>
2024-01-07 23:11:15 -0600

    Can now easily add projects

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
-import profile from  './Screenshots/profile.png';
+import profile from './Screenshots/profile.png';
 
 export default function Portfolio() {
   const [showForm, setShowForm] = useState(false);
@@ -18,12 +18,12 @@ export default function Portfolio() {
 
     try {
       await emailjs.sendForm(
-        //service_ID, template_ID, e.target, user_ID
         "service_ID80067",
         "template_c5xi21q",
         e.target,
         "0lf4MTEWnTCOzmStD"
       );
+
       // Reset form fields
       e.target.reset();
       alert("Email sent successfully!");
@@ -39,38 +39,46 @@ export default function Portfolio() {
       <div className="ImageContainer">
         <header>Ashara Martinez</header>
         <h1>New Journey New Knowledge</h1>
-        <img src={profile} alt="Ashara-PF"/>
+        <img src={profile} alt="Ashara-PF" />
         <section id="about">
           <h2>About Me</h2>
           <p>
-            Hi, my name is Ashara Martinez, and I'm a Full Stack Web Dev. I am admittedly new to this field in tech but I was introduced via a bootcamp I took online with Washington University of Saint Louis. I like to focus on the front-end aspects of development for the most part as I feel like I need more practice with the speed in which I can implement the back-end aspects of a website. I'm well-versed in CSS, HTML, JavaScript, React, MYSQL, MongoDB, and Node.js, Express, and JWT. I've also used JEST, AJAX, Sequelize, JSON, and a few others.
+            Hi, my name is Ashara Martinez, and I'm a Full Stack Web Dev. I am admittedly new to this field in tech but I was introduced via a boot camp I took online with Washington University of Saint Louis. I like to focus on the front-end aspects of development for the most part but I can do back-end as well albeit at a slower pace. I'm well-versed in CSS, HTML, JavaScript, React, MYSQL, MongoDB, and Node.js, Express, and JWT. I've also used JEST, AJAX, Sequelize, JSON, and a few others. If you wish to contact me simply shoot me an email via the contact form below. Thank you for visiting my portfolio.
           </p>
         </section>
       </div>
 
       {/* Contact Me Section */}
-      <h2>Repository</h2>
-      <section id="contact-me">
-        <p>
-          <a href="https://github.com/Amartinez2075">
-            <span className="GitHub">Visit My GitHub Repository</span>
-          </a>
-        </p>
+      <section id="contact">
         {!showForm ? (
           <button id="showFormBtn" onClick={handleShowForm}>
-            Show Contact Form
+            Open Contact Form
           </button>
         ) : (
           <div>
-            <form id="contactForm" onSubmit={handleSubmit}>
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" required />
-              <label htmlFor="email">Email:</label>
-              <input type="email" id="email" name="email" required />
-              <label htmlFor="message">Message:</label>
-              <textarea id="message" name="message" required></textarea>
-              <button type="submit">Send Message</button>
-            </form>
+          <form id="contactForm" onSubmit={handleSubmit}>
+  <div className="form-group">
+    <label htmlFor="name">Name:</label>
+    <div></div>
+    <input type="text" id="name" name="name" required />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="email">Email:</label>
+    <div></div>
+    <input type="email" id="email" name="email" required />
+  </div>
+
+  <div className="form-group">
+    <label htmlFor="message">Message:</label>
+    <div></div>
+    <textarea id="message" name="message" required></textarea>
+  </div>
+
+  <button type="submit">Send Message</button>
+  <div></div>
+</form>
+
             <button id="closeFormBtn" onClick={handleCloseForm}>
               Close Contact Form
             </button>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,55 +1,144 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './CSS/Project.css';
 
-const Project = ({ title, description, imageUrl, githubLink, deployedLink }) => {
+const ProjectItem = ({ title, description, githubLink, deployedLink, tools }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleToggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
-    <div>
-    {/* Personal Projects Section */}
-    <div>
-    <section id="projects">
-    <h1>Personal Projects</h1>
-    <div className="project-container">
-    <p>
-    <a href="https://steveb175.github.io/Brewery-Finder/">
-    <span className="project-rectangle-A">Brewery Finder</span>
-    </a>
-    </p>
-    <p>
-    <a href="https://omicron-5498.herokuapp.com/">
-    <span className="project-square-A">Front End Employee Tracker</span>
-    </a>
-    </p>
-    <p>
-    <a href="https://amartinez2075.github.io/Coding-Quiz-3/">
-    <span className="project-square-B">Coding Quiz</span>
-    </a>
-    </p>
-    <p>
-    <a href="https://amartinez2075.github.io/Work-Day-Scheduler/">
-    <span className="project-square-C">Work-Day-Scheduler</span>
-    </a>
-    </p>
-    <p>
-    <a href="https://github.com/Amartinez2075/Employee-Tracker">
-    <span className="project-square-D">Employee Tracker</span>
-    </a>
-    </p>
-    <p>
-    <a href="https://saint-louis-pets-554-d862dc3f7739.herokuapp.com/">
-    <span className="project-rectangle-A">Saint Louis Pets</span>
-    </a>
-    </p>
-    </div>
-    </section>
-    </div>
-      {/* Orange Section */}
-      <section id="Thick-section">
-      {/* Orange Section content here */}
-    </section>
-    <footer>
-    
-    </footer>  
+    <div className={`project-item ${expanded ? 'expanded' : ''}`}>
+      <p onClick={handleToggleExpand}>
+        <span className="project-title">{title}</span>
+      </p>
+      {expanded && (
+        <div className="project-details">
+          {tools && (
+            <p>
+              <strong>Tools: </strong> {tools}
+            </p>
+          )}
+          <p>{description}</p>
+          <p>
+            <strong>GitHub Repository:</strong>{' '}
+            <a href={githubLink} target="_blank" rel="noopener noreferrer">
+              {githubLink}
+            </a>
+          </p>
+          {deployedLink && (
+            <p>
+              <strong>Deployed Link:</strong>{' '}
+              <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+                {deployedLink}
+              </a>
+            </p>
+          )}
+        </div>
+      )}
     </div>
   );
-  }
+};
+
+const Project = () => {
+  const projects = [
+    {
+      id: 1,
+      title: 'No-React Website Portfolio',
+      description: 'Basic Website Portfolio with CSS and HTML.',
+      githubLink: 'https://github.com/Amartinez2075/Website-Porfolio',
+      deployedLink: 'https://amartinez2075.github.io/Website-Porfolio/',
+    },
+    {
+      id: 2,
+      title: 'Coding Quiz',
+      description: 'This is a quiz about coding, with 5 questions total.',
+      githubLink: 'https://github.com/Amartinez2075?tab=repositories',
+      deployedLink: 'https://amartinez2075.github.io/Coding-Quiz-3/',
+    },
+    // Add more project objects as needed
+    {
+      id: 3,
+      title: 'Work Day Scheduler',
+      description: 'Simple Work day Schedule and saves the user data.',
+      githubLink: 'https://github.com/Amartinez2075/Work-Day-Scheduler',
+      deployedLink: 'https://amartinez2075.github.io/Work-Day-Scheduler/',
+    },
+    {
+      id: 4,
+      title: 'Brewery Finder',
+      description:
+        'A Brewery Finder Application that tells the weather, location of a place, and other info.',
+      githubLink: 'https://github.com/Amartinez2075/Brewery-Finder',
+      deployedLink: 'https://amartinez2075.github.io/Brewery-Finder/',
+      tools: 'React, CSS, API',
+    },
+    {
+      id: 5,
+      title: 'Employee Tracker',
+      description: 'A backend MySQL database, allows user to make, delete, and update employees, and more.',
+      githubLink: 'https://github.com/Amartinez2075/Employee-Tracker',
+      tools: 'Node.js, MySQL, Inquirer',
+    },
+    {
+      id: 6,
+      title: 'Ecommerce Backend',
+      description: 'MySQL database that allows user to make, delete, and update products, tags, and categories.',
+      githubLink: 'https://github.com/Amartinez2075/E-Commerce-Back-End',
+      tools: 'Node.js, Express.js, Sequelize, MySQL',
+    },
+    {
+      id: 7,
+      title: 'Model View Controller Tech Blog',
+      description: 'A blog that allows users to make, delete, and update posts, and comments',
+      githubLink: 'https://github.com/Amartinez2075/Model-View-Controller-Tech-Blog',
+      deployedLink: 'https://model-view.herokuapp.com/',
+      tools: 'Node.js, Express.js, MySQL, Sequelize, Handlebars',
+    },
+    {
+      id: 8,
+      title: 'Front and Back End Employee Tracker',
+      description:
+        'A front and back end employee tracker that allows users to make, delete, and update employees, and more',
+      githubLink: 'https://github.com/Amartinez2075/Website-Porfolio',
+      deployedLink: 'https://omicron-5498.herokuapp.com/',
+      tools: 'Node.js, Express.js, MySQL, Sequelize, Handlebars, JavaScript, HTML, CSS',
+    },
+    {
+      id: 9,
+      title: 'Progressive Web Application Text Editor',
+      description:
+        'This is a text editor that allows users to make, delete, and update text, and saves it to the cache for offline use.',
+      githubLink: 'https://github.com/Amartinez2075/Progressive-Web-Applications--PWA--Challenge-Text-Editor#table-of-contents',
+      deployedLink: 'https://text-editor-pls-c273a9bf7b03.herokuapp.com/',
+      tools: 'Node.js, Express.js, IndexedDB, Service Worker',
+    },
+  ];
+
+  return (
+    <div>
+      <section id="projects">
+        <header>Personal Projects</header>
+        <div className="Github-container">
+          <h2>Repository</h2>
+          <section id="contact-me">
+            <p>
+              <a href="https://github.com/Amartinez2075">
+                <span className="GitHub">Visit My GitHub Repository</span>
+              </a>
+            </p>
+          </section>
+          <div className="project-container">
+            {projects.map((project) => (
+              <ProjectItem key={project.id} {...project} />
+            ))}
+          </div>
+        </div>
+      </section>
+      <footer></footer>
+    </div>
+  );
+};
 
 export default Project;
