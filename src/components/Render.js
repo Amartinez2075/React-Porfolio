7f1a549de77b84a774d01be2bfbdb6fe2c108989
Ashara Martinez <Asharamart2075@gmail.com>
2024-01-08 19:30:36 -0600

    started adjusting stuff for smaller screens

--- src/components/Render.js
+++ src/components/Render.js
@@ -1,32 +1,20 @@
 import React, { useState } from 'react';
 import About from './About.js';
-import Skills from './Skills.js';
 import Projects from './Projects.js';
-import Menubar from './Menubar.js';
-import Resume from './Resume.js';
-import ContactMe from './ContactMe.js';
+import Navigation from 'Navigation.js';
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
 
-  // TODO: Add a comment describing the functionality of this method
+
    // Method to render the appropriate page component based on the value of currentPage
   const renderPage = () => {
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Skills') {
-      return <Skills />;
-    }
     if (currentPage === 'Projects') {
       return <Projects />;
-    }
-    if (currentPage === 'ContactMe') {
-      return <ContactMe />;
-    }
-    if (currentPage === 'Resume') {
-      return <Resume />;
-    }
+    } 
      // Default case: Render the Contact component if none of the above conditions are met
     return <About />;
   };
@@ -37,7 +25,7 @@ export default function PortfolioContainer() {
   return (
     <div>
       {/* Pass the currentPage and handlePageChange function as props to the NavTabs component */}
-      <Menubar currentPage={currentPage} handlePageChange={handlePageChange} />
+      <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
        {/* Render the appropriate page component based on the value of currentPage */}
       {renderPage()}
     </div>
